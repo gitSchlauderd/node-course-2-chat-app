fe129b40bab5a127eddd3e4b7bc0e0c932d05677
Jeff <jcschlauder@gmail.com>
2018-01-14 19:47:42 -0700

    Add Users class and updateUserList event

---
+++ server/utils/validation.test.js
@@ -0,0 +1,19 @@
+const expect = require('expect');
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+  it('should reject non-string values', () => {
+    var res = isRealString(99);
+    expect(res).toBe(false);
+  });
+
+  it('should reject string with only spaces', () => {
+    var res = isRealString('       ');
+    expect(res).toBe(false);
+  });
+
+  it('should allow strings with non-space characters', () => {
+    var res = isRealString('  Jeff  ');
+    expect(res).toBe(true);
+  });
+});
